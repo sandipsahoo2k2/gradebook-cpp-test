2d09ddcf48b1d65905f5c5255ee367b765923f39
Sandeep <sandeep@Sandeeps-MacBook-Pro.local>
2015-12-12 22:50:21 +0530

    main.cpp fixed for allowing any order input

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,26 @@ void calculateGrade(GradeBook& gradeBook, std::string& studentName, std::string&
 int main(int argc, char *argv[])
 {
         AllocationManager allocationManager;
+	StudentManager studentManager;
+	StudentGradesManager studentGradesManager(studentManager);
+	GradeBook gradeBook(allocationManager, studentManager, studentGradesManager);
+
+	std::cout << "\nCREATE STUDENT DB\n";
+
 	std::string inputString = "Y" ;
-	while ("Y" == inputString)
+	while ("Y" == inputString || "y" == inputString)
+	{
+		std::cout << "Please enter the student name to be added: ";
+		std::string studentName;
+		std::cin >> studentName;
+        	studentManager.addStudent(studentName);
+		std::cout << "Do you want to continue[Y|N]? ";
+		std::cin >> inputString;
+	}
+
+	std::cout << "\nCREATE ALLOCATIONS DB\n";
+	inputString = "Y" ;
+	while ("Y" == inputString || "y" == inputString)
 	{ 
 		std::cout << "Please enter the allocations informations\n";
 		std::cout << "Teacher Name:";
@@ -38,63 +56,70 @@ int main(int argc, char *argv[])
 		std::cin >> inputString;
 	}
 
-	StudentManager studentManager;
-	StudentGradesManager studentGradesManager(studentManager);
-	GradeBook gradeBook(allocationManager, studentManager, studentGradesManager);
 
-	std::cout << "Please enter the teacher name for grade calculation:";
+	std::cout << "\nCALCULATE GRADE FOR STUDENT BY TEACHER\n";
+
+	std::cout << "Note : Without the teacher and student name provided grade can not be calculated on the fly!" <<std::endl;
+	std::cout << "Please enter the teacher name:";
         std::string teacherName;
         std::cin >> teacherName;
+	AllocationPtr allocation = allocationManager.getAllocation(teacherName);
+	if(!allocation)
+	{
+		std::cout << "Allocation information not found for: " << teacherName << std::endl;
+		std::cout << "\nAPPLICATION GRADEBOOK STOPS\n";
+		return -1;
+	}
+
+	std::cout << "Please enter the student name:";
+        std::string studentName;
+        std::cin >> studentName;
+	StudentPtr student = studentManager.getStudent(studentName);
+	if(!student)
+	{
+		std::cout << "Student information not found for: " << studentName << std::endl;
+		std::cout << "\nAPPLICATION GRADEBOOK STOPS\n";
+		return -1;
+	}
 
 	inputString = "Y";
-	while ("Y" == inputString)
+	int option = 0 ;
+	std::cin.clear();	
+	std::cout << "Please choose option Assignment:1, Exam:2 or OptionalAssignment/Extra Credit:3 for entering score" << std::endl;
+	std::cout << "Select [1|2|3] or 0 to discontinue:" ;
+	std::cin >> option ;
+	while (option != 0)
 	{
-		std::cout << "Please enter the student name to be added: ";
-		std::string studentName;
-		std::cin >> studentName;
-        	studentManager.addStudent(studentName);
-		std::cout << "Do you want to continue[Y|N]? ";
-		std::cin >> inputString;
-
-		inputString = "Y" ;
-		while ("Y" == inputString)
+		if(option == 1)
 		{
 			std::cout << "Please enter the score for Assignment: ";
 			double score;
 			std::cin >> score;
 			studentGradesManager.addGradeScore(studentName, E_ASSIGNMENT, score);
-			std::cout << "Do you want to add more [Y|N]? ";
-			std::cin >> inputString;
 			calculateGrade(gradeBook, studentName, teacherName);
 		}
-
-		inputString = "Y" ;
-                while ("Y" == inputString)
-                {
-                        std::cout << "Please enter [1|0] if optional assignement is completed:";
-                        int completed;
-                        std::cin >> completed;
+		else if(option == 2)
+		{
+			std::cout << "Please enter the score for Exam: ";
+			double score;
+			std::cin >> score;
+			studentGradesManager.addGradeScore(studentName, E_EXAM, score);
+			calculateGrade(gradeBook, studentName, teacherName);
+		}
+		else if(option == 3)
+		{
+			std::cout << "Please enter [1|0] if optional assignement is completed:";
+			int completed;
+			std::cin >> completed;
 			if(completed)
 			{
 				studentGradesManager.addGradeScore(studentName, E_OPTIONAL_ASSIGNMENT);
 			}
-                        std::cout << "Do you want to add more [Y|N]? ";
-                        std::cin >> inputString;
 			calculateGrade(gradeBook, studentName, teacherName);
-                }
-
-		inputString = "Y" ;
-                while ("Y" == inputString)
-                {
-                        std::cout << "Please enter the score for Exam: ";
-                        double score;
-                        std::cin >> score;
-                        studentGradesManager.addGradeScore(studentName, E_EXAM, score);
-                        std::cout << "Do you want to add more [Y|N]? ";
-                        std::cin >> inputString;
-			calculateGrade(gradeBook, studentName, teacherName);
-                }
+		}
+		std::cout << "To continue choose option [Assignment:1, Exam:2 or OptionalAssignment:3] or 0 to discontinue:" ;
+		std::cin >> option ;
 	}
-
+	std::cout << "\nAPPLICATION GRADEBOOK STOPS\n";
 	return 0;
 }
